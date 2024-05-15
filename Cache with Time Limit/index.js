ec178a92312ba0b4e5041cba2600ddfc4323a682
Amanpreet Kaur <amanpreet8507@gmail.com>
2024-05-15 16:48:20 -0700

    day 21

--- Cache with Time Limit/index.js
+++ Cache with Time Limit/index.js
@@ -81,31 +81,6 @@ class TimeLimitedCache {
   }
 }
 
-// /**
-//  * @param {number} key
-//  * @param {number} value
-//  * @param {number} duration time until expiration in ms
-//  * @return {boolean} if un-expired key already existed
-//  */
-// TimeLimitedCache.prototype.set = function (key, value, duration) {
-//   const currentTime = Date.now();
-//   if (this.data[key] && this.data[key.expiration] > currentTime) {
-//     this.data[key].value = value;
-//     this.data[key].expiration = currentTime + duration;
-//     return true;
-//   }
-// };
-
-// /**
-//  * @param {number} key
-//  * @return {number} value associated with key
-//  */
-// TimeLimitedCache.prototype.get = function (key) {};
-
-// /**
-//  * @return {number} count of non-expired keys
-//  */
-// TimeLimitedCache.prototype.count = function () {};
 
 const timeLimitedCache = new TimeLimitedCache();
 console.log(timeLimitedCache.set(1, 42, 1000)); // false
